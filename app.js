2381c0c0da8e46701d7b557e54b8e3d7fa445ff1
Bazthos <100133690+Bazthos@users.noreply.github.com>
2022-04-11 08:56:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-let questions = document.querySelectorAll('.passiv_question');
+let blocs = document.querySelectorAll('.bloc');
+let faqs = document.querySelector('.faq');
 
-function faq(){
-    questions.forEach((ele, i) => {
-        if(ele.getAttribute('class') === ''){
-            ele.classList.add('passiv_question')
-        };
-        ele.addEventListener('click', e => {
-            document.querySelector('.active_question')?.classList.add('passiv_question');
-            document.querySelector('.active_answer')?.classList.add('passiv_answer');
-            document.querySelector('.active_answer')?.classList.remove('active_answer');
-            document.querySelector('.active_question')?.classList.remove('active_question');
-            document.querySelector('.active_arrow')?.classList.remove('active_arrow');
-            ele.classList.remove('passiv_question');
-            ele.setAttribute('class','active_question');
-            ele.querySelector('.arrow').classList.add('active_arrow');
-            ele.nextElementSibling.classList.remove('passiv_answer');
-            ele.nextElementSibling.classList.add('active_answer');
-        })}
-);}
-
-faq();
+faqs.addEventListener('click', e => {
+    const targetBox = e.target.closest('.bloc').classList;
+    if(targetBox.contains('active')){
+        targetBox.remove('active');
+    }else{
+        blocs.forEach(bloc => bloc.classList.remove('active'));
+        targetBox.add('active');
+    }
+})
